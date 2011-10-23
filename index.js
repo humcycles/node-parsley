610bb6de6ce3cd9c5d31d45011104524d7ae4b03
James Halliday <mail@substack.net>
2011-10-23 03:43:20 -0700

    simple get test passes

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-var EventEmitter = require('events').EventEmitter;
-var http = require('http');
-var IncomingMessage = http.IncomingMessage;
-
-module.exports = function (stream) {
-    return new Parser(stream);
+module.exports = function (stream, cb) {
+    return new Parser(stream, cb);
 };
 
-function Parser (stream) {
+function Parser (stream, cb) {
     var self = this;
+    self.stream = stream;
+    self.cb = cb;
     
     stream.on('data', function (buf) {
         self.execute(buf, 0, buf.length);
     });
     
     this.mode = 'begin';
-    this.request = new IncomingMessage(stream);
 }
-
-Parser.prototype = new EventEmitter;
 
 Parser.prototype.execute = function (buf, start, len) {
     for (var i = start; i < len && i >= 0; ) {
         i = this.modes[this.mode].call(this, buf, i, len - i);
+        if (i < 0) console.error('error parsing ' + this.mode);
     }
 };
 
-Parser.prototype.modes = require('./modes');
+Parser.prototype.modes = require('./lib/modes');
