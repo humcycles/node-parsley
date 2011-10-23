9c504d0ddc329008c3e93562b9105008c232789f
James Halliday <mail@substack.net>
2011-10-23 06:49:28 -0700

    failing keep-alive test

---
+++ test/keep_alive.js
@@ -0,0 +1,74 @@
+var test = require('tap').test;
+var parsley = require('../');
+var chunky = require('chunky');
+var Stream = require('net').Stream;
+
+test('get', function (t) {
+    var pending = 50;
+    t.plan(50 * 2);
+    
+    Array(50+1).join('x').split('').forEach(function () {
+        var stream = new Stream;
+        stream.readable = true;
+        
+        var i = 0;
+        parsley(stream, function (req) {
+            var rh = [], rb = [];
+            
+            req.on('rawHead', function (buf) {
+                rh.push(buf);
+            });
+            
+            req.on('rawBody', function (buf) {
+                rb.push(buf);
+            });
+            
+            req.on('headers', function (headers) {
+                t.equal(req.url, [ '/first', '/second' ][i]);
+                
+                t.deepEqual(headers, {
+                    host : [ 'beep', 'boop' ][i],
+                });
+            });
+            
+            req.on('end', function () {
+                t.deepEqual(rb, []);
+                
+                t.equal(
+                    rh.map(String).join(''),
+                    [
+                        'GET /' + req.url + ' HTTP/1.1',
+                        'Host: ' + req.headers.host,
+                        '',
+                        ''
+                    ].join('\r\n')
+                );
+                
+                if (++i === 2) {
+                    if (--pending === 0) {
+                        t.end();
+                    }
+                }
+            });
+        });
+        
+        var msg = new Buffer([
+            'GET /first HTTP/1.1',
+            'Host: beep',
+            '',
+            'GET /second HTTP/1.1',
+            'Host: boop',
+            '',
+            ''
+        ].join('\r\n'));
+        
+        var chunks = chunky(msg);
+        var iv = setInterval(function () {
+            stream.emit('data', chunks.shift());
+            if (chunks.length === 0) {
+                stream.emit('end');
+                clearInterval(iv);
+            }
+        }, 10);
+    });
+});
